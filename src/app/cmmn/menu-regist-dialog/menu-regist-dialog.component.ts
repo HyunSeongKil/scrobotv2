4e653b0207205b02aed4650bd9f589aaa10f3fc7
SeongKil <38479834+HyunSeongKil@users.noreply.github.com>
2021-11-18 23:50:35 +0900

    메뉴관리

--- src/app/cmmn/menu-regist-dialog/menu-regist-dialog.component.ts
+++ src/app/cmmn/menu-regist-dialog/menu-regist-dialog.component.ts
@@ -1,15 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, Subject } from 'rxjs';
+import { Scrobot } from 'src/app/@types/scrobot';
+import { MenuService } from 'src/app/service/menu.service';
+import { ScrinService } from 'src/app/service/scrin.service';
 
 @Component({
   selector: 'app-menu-regist-dialog',
   templateUrl: './menu-regist-dialog.component.html',
-  styleUrls: ['./menu-regist-dialog.component.css']
+  styleUrls: ['./menu-regist-dialog.component.css'],
 })
 export class MenuRegistDialogComponent implements OnInit {
+  @ViewChild('content') content!: ElementRef<HTMLDivElement>;
 
-  constructor() { }
+  @Output() initedEvent = new EventEmitter<any>();
+  @Output() registingEvent = new EventEmitter<any>();
+  @Output() registedEvent = new EventEmitter<any>();
 
-  ngOnInit(): void {
+  private closeResult = '';
+
+  scrins: Scrobot.Scrin[] = [];
+
+  form!: FormGroup;
+
+  constructor(private modalService: NgbModal, private service: MenuService, private scrinService: ScrinService) {
+    this.form = new FormGroup({
+      prjctId: new FormControl('', [Validators.required]),
+      prntsMenuId: new FormControl('', [Validators.required]),
+      menuNm: new FormControl('', [Validators.required]),
+      scrinId: new FormControl('', []),
+    });
   }
 
+  ngOnInit(): void {
+    this.initedEvent.emit(this);
+  }
+
+  /**
+   * 모달 창 실행
+   */
+  open(prjctId: string, prntsMenuId: string = '-') {
+    this.form.patchValue({ prjctId, prntsMenuId, menuNm: '' });
+    this.scrins = [];
+
+    //  화면 목록 조회
+    this.scrinService.listByPrjctId(prjctId).then((res: any) => {
+      this.scrins = res.data;
+    });
+
+    this.modalService.open(this.content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      (result: any) => {
+        this.closeResult = `Closed with: ${result}`;
+
+        if ('SAVE' === result) {
+          if (!this.form.valid) {
+            alert('입력값을 확인하시기 바랍니다.');
+            this.open(prjctId);
+            return;
+          }
+
+          if (!confirm('저장하시겠습니까?')) {
+            return;
+          }
+
+          this.registingEvent.emit('');
+
+          //
+          this.service.regist(this.form.value as Scrobot.Menu).then((res: any) => {
+            this.registedEvent.emit('');
+          });
+        }
+      },
+      (reason: any) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
--- src/app/editor2/menu2/menu2.component.ts
+++ src/app/editor2/menu2/menu2.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Host, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Host, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Scrobot } from 'src/app/@types/scrobot';
+import { MenuRegistDialogComponent } from 'src/app/cmmn/menu-regist-dialog/menu-regist-dialog.component';
+import { MenuUpdtDialogComponent } from 'src/app/cmmn/menu-updt-dialog/menu-updt-dialog.component';
 import { MenuService } from 'src/app/service/menu.service';
 import { Edit2Component, TabSe } from '../edit2/edit2.component';
 
@@ -10,6 +12,9 @@ import { Edit2Component, TabSe } from '../edit2/edit2.component';
   styleUrls: ['./menu2.component.css'],
 })
 export class Menu2Component implements OnInit, OnDestroy {
+  @ViewChild('menuRegistDialogRef') menuRegistDialogRef!: MenuRegistDialogComponent;
+  @ViewChild('menuUpdtDialogRef') menuUpdtDialogRef!: MenuUpdtDialogComponent;
+
   @Output() initedEvent = new EventEmitter<any>();
 
   hostComponent: Edit2Component | undefined = undefined;
@@ -17,6 +22,14 @@ export class Menu2Component implements OnInit, OnDestroy {
   tabChangedEventSub: Subscription | undefined = new Subscription();
   editingEventSub: Subscription | undefined = new Subscription();
   closeEditingEventSub: Subscription | undefined = new Subscription();
+
+  //
+  menuRegistingEventSub: Subscription | undefined = new Subscription();
+  menuRegistedEventSub: Subscription | undefined = new Subscription();
+
+  //
+  menuUpdtingEventSub: Subscription | undefined = new Subscription();
+  menuUpdtedEventSub: Subscription | undefined = new Subscription();
 
   menus: Scrobot.Menu[] = [];
 
@@ -28,12 +41,20 @@ export class Menu2Component implements OnInit, OnDestroy {
       this.tabChangedEventSub?.unsubscribe();
     }
 
+    //
     if (!this.editingEventSub?.closed) {
       this.editingEventSub?.unsubscribe();
     }
-
     if (!this.closeEditingEventSub?.closed) {
       this.closeEditingEventSub?.unsubscribe();
+    }
+
+    //
+    if (!this.menuRegistingEventSub?.closed) {
+      this.menuRegistingEventSub?.unsubscribe();
+    }
+    if (!this.menuRegistedEventSub?.closed) {
+      this.menuRegistedEventSub?.unsubscribe();
     }
   }
 
@@ -42,9 +63,7 @@ export class Menu2Component implements OnInit, OnDestroy {
 
     this.tabChangedEventSub = this.hostComponent?.tabChangedEvent.subscribe((se: string) => {
       if (TabSe.Menu === se) {
-        this.service.listByPrjctId(this.hostComponent?.prjctId ?? '').then((res: any) => {
-          this.menus = res.data;
-        });
+        this.getData();
       }
     });
 
@@ -53,9 +72,65 @@ export class Menu2Component implements OnInit, OnDestroy {
     this.closeEditingEventSub = this.hostComponent?.closeEditingEvent.subscribe((scrinId: string) => {});
   }
 
-  openMenuRegistDialog(menuId: string): void {}
+  getData(): void {
+    this.service.listByPrjctId(this.hostComponent?.prjctId ?? '').then((res: any) => {
+      this.menus = res.data;
+    });
+  }
 
-  openMenuUpdateDialog(menuId: string): void {}
+  /**
+   * 메뉴등록 콤포넌트 초기화 완료
+   * @param a 메뉴등록 콤포넌트 인스턴스
+   */
+  menuRegistInitedEvent(a: MenuRegistDialogComponent): void {
+    this.menuRegistDialogRef = a;
 
-  deleteMenu(menuId: string): void {}
+    this.menuRegistedEventSub = this.menuRegistDialogRef.registedEvent.subscribe(() => {
+      this.getData();
+    });
+    this.menuRegistingEventSub = this.menuRegistDialogRef.registingEvent.subscribe(() => {});
+  }
+
+  /**
+   * 메뉴수정 콤포넌트 초기화 완료
+   * @param a 메뉴수정 콤포넌트 인스턴스
+   */
+  menuUpdtInitedEvent(a: MenuUpdtDialogComponent): void {
+    this.menuUpdtDialogRef = a;
+
+    this.menuUpdtedEventSub = this.menuUpdtDialogRef.updtedEvent.subscribe(() => {
+      this.getData();
+    });
+    this.menuUpdtingEventSub = this.menuUpdtDialogRef.updtingEvent.subscribe(() => {});
+  }
+
+  openMenuRegistDialog(menuId: string): void {
+    this.menuRegistDialogRef.open(this.hostComponent?.prjctId ?? '', menuId);
+  }
+
+  openMenuUpdateDialog(menuId: string, prntsMenuId?: string): void {
+    this.menuUpdtDialogRef.open(this.hostComponent?.prjctId ?? '', menuId, prntsMenuId);
+  }
+
+  deleteMenu(menuId: string): void {
+    // 하위 메뉴 존재 검사
+    if (this.existsChildren(menuId)) {
+      alert('하위 메뉴 삭제 후 다시 시도하시기 바랍니다.');
+      return;
+    }
+
+    if (!confirm('삭제하시겠습니까?')) {
+      return;
+    }
+
+    this.service.delete(menuId).then(() => {
+      this.getData();
+    });
+  }
+
+  existsChildren(prntsMenuId: string): boolean {
+    const menu: Scrobot.Menu | undefined = this.menus.find((x) => x.prntsMenuId === prntsMenuId);
+
+    return undefined !== menu ? true : false;
+  }
 }
